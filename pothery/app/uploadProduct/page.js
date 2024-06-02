d8458bc02d44688865c99e2d84df0e88ac2ba8ec
Tsz Kit Lo (Albert) <tlo20@myseneca.ca>
2024-06-02 17:02:47 -0400

    add side menu

---
+++ pothery/app/uploadProduct/page.js
@@ -0,0 +1,14 @@
+import Sidemenu from "@/components/SideMenu";
+
+export default function uploadProject(){
+    return (
+        <div className="w-11/12 mx-auto min-h-screen">
+            <div className="flex">
+                <Sidemenu/>
+                <div className="w-8/12">
+
+                </div>
+            </div>
+        </div>
+    )
+}
