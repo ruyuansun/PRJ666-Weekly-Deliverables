4d3d8ca129a9430f43d3c52a830f6bb00d7983f5
ruyuansun <rsun35@myseneca.ca>
2024-06-02 05:34:04 -0400

    Integrate register and login pages with backend API

--- pothery/app/login/page.js
+++ pothery/app/login/page.js
@@ -1,4 +1,41 @@
+"use client";
+import React, { useState } from "react";
+
 export default function Login() {
+  const [error, setError] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setError("");
+    const username = event.target.username.value;
+    const password = event.target.password.value;
+
+    try {
+      const response = await fetch("http://localhost:4000/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
+
+      const data = await response.json();
+      if (data.message === "Login successful") {
+        alert("Login successful!");
+      } else if (data.message === "Incorrect username or password") {
+        setError("Incorrect username or password");
+      } else {
+        setError("Login failed: " + data.message);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setError("An error occurred during login.");
+    }
+  }
+
   return (
     <div className="text-center">
       <h1 className="text-5xl font-medium mb-3">Sign In</h1>
@@ -17,7 +54,11 @@ export default function Login() {
         </a>
       </p>
 
-      <form className="flex flex-col w-96 mx-auto space-y-4" method="post">
+      <form
+        className="flex flex-col w-96 mx-auto space-y-4"
+        onSubmit={handleSubmit}
+      >
+        {error && <p className="text-red-500">{error}</p>}
         <input
           className="border rounded-md py-2 px-3 w-96"
           type="text"
