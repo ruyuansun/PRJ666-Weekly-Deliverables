0d2aa1f73b0710891c2ab8965e93ad906515435b
Joe Peralta <jperalta11@myseneca.ca>
2024-06-17 22:32:41 -0400

    Implemented front end for marketplace page

---
+++ pothery/app/marketplace/page.js
@@ -0,0 +1,26 @@
+"use client";
+
+import Sidemenu from "../../components/SideMenu";
+import Product from "../../components/Product";
+import { useState } from "react";
+
+export default function Marketplace() {
+  const [profPhotoUrl, setProfPhotoUrl] = useState("/placeholders/profile.jpg");
+  const [bgImageUrl, setBgImageUrl] = useState(
+    "/placeholders/background-image.jpg"
+  );
+
+  return (
+    <div className="w-11/12 mx-auto min-h-screen">
+      <div className="flex">
+        <Sidemenu />
+        <div className="w-full px-10 grid grid-cols-2 gap-10">
+          <Product userProfileUrl={profPhotoUrl} postImageUrl={bgImageUrl} />
+          <Product userProfileUrl={profPhotoUrl} postImageUrl={bgImageUrl} />
+          <Product userProfileUrl={profPhotoUrl} postImageUrl={bgImageUrl} />
+          <Product userProfileUrl={profPhotoUrl} postImageUrl={bgImageUrl} />
+        </div>
+      </div>
+    </div>
+  );
+}
