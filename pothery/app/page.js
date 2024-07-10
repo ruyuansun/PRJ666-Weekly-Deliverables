45f7a21377a81321d77ee6fe856d8f98884fc914
Joe Peralta <jperalta11@myseneca.ca>
2024-07-10 10:49:19 -0400

    Added sidemenu component in the homepage

--- pothery/app/page.js
+++ pothery/app/page.js
@@ -1,5 +1,14 @@
+"use client";
+
 import Image from "next/image";
+import SideMenu from "../components/SideMenu";
 
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="w-11/12 mx-auto min-h-screen">
+      <div className="flex">
+        <SideMenu />
+      </div>
+    </div>
+  );
 }
