294f208f868e53cded3abd43d256de080b2e97b7
Tsz Kit Lo (Albert) <tlo20@myseneca.ca>
2024-08-01 18:08:14 -0400

    add post

--- pothery_server/src/routes/posts.js
+++ pothery_server/src/routes/posts.js
@@ -9,6 +9,7 @@ module.exports = {
 
 function post_routes(router) {
   post_get(router);
+  post_add(router);
   return;
 }
 
@@ -24,7 +25,6 @@ function post_get(router) {
                   FROM post p
                   LEFT JOIN users u
                   ON p.user_id = u.id
-                  
                   `
 
     db.query(query)
@@ -39,4 +39,29 @@ function post_get(router) {
       });
   });
 
+}
+
+
+function post_add(router) {
+  router.post("/addPost", crypto.authorize_token, (req, res) => {
+    let token_data = crypto.decode_token(req);
+
+    let { img_url,content } = req.body;
+    if (img_url=="" || img_url==undefined || content=="" || content==undefined)
+      {res.status(500).json({ error: "All fields must be provided" });return}
+
+    const query =
+      "INSERT INTO post (user_id, img_url, content) VALUES ( ?, ?, ?)";
+    db.query(query, [token_data.uid, img_url, content])
+      .then((result) => {
+        if (result) {
+          res.status(200).json({ message: result });
+        }
+      })
+      .catch((err) => {
+        console.error("Database error:", err);
+        res.status(500).json({ error: err.message });
+      });
+  });
+  return;
 }
